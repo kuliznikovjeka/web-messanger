260d3e9d576ac5ec072272f0d2c39138b6aab853
kuliznikovjeka <99824934+kuliznikovjeka@users.noreply.github.com>
2024-01-11 21:36:49 +0300

    Add files via upload

--- js/ui-elements.js
+++ js/ui-elements.js
@@ -2,6 +2,6 @@ export const modalElements = {
 	btnSettings: document.querySelector('.top-actions-communication__btn-settings'),
 	modalOverlay: document.querySelector('.modal-overlay'),
 	setingsModal: document.querySelector('.settings-modal'),
-	btnCloseModal: document.querySelector('.top-modal-settings__exit'),
+	btnCloseModal: document.querySelector('.top-modal-settings__close'),
 }
 
